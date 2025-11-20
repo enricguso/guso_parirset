78c540bdfaa1c50ca68d7e85eb247b412e905eed
Enric Gusó Muñoz <enricguso@gmail.com>
2025-11-20 18:03:27 +0100

    merged fsd crawler and downloader

--- fsd_downloader.py
+++ fsd_downloader.py
@@ -27,52 +27,87 @@ client = freesound.FreesoundClient()
 client.set_token(oauth_token["access_token"], "oauth")
 client.session = LimiterSession(per_minute=59)
 
-df = pd.read_csv('fsd_crowd_sounds.csv')
+
+words = ['crowd', 'applause', 'audience', 'cheering', 'chatter', 'protest']
+
+licenses = ['http://creativecommons.org/licenses/by/3.0/',
+ 'http://creativecommons.org/publicdomain/zero/1.0/',
+ 'https://creativecommons.org/licenses/by-nc/4.0/',
+ 'https://creativecommons.org/licenses/by/4.0/']
 
 
-all_words = df.word.unique().tolist()
-for all_word in all_words:
-    if not os.path.exists('downloads/'+all_word):
-        os.makedirs('downloads/'+all_word)
-df.rating_pos = df.rating_pos // 2
-downloaded = [False]*len(df)
-filenames = [None] * len(df)
-df['filenames'] = filenames
-df['downloaded'] = downloaded
+data = []
+ids_list = []
+i = 0
+if os.path.exists('fsd_crowd_sounds.xlsx'):
+    print('Loading existing data...')
+    df_existing = pd.read_excel('fsd_crowd_sounds.xlsx')
+    for i in range(len(df_existing)):
+        data.append({
+                    'id': df_existing.loc[i, 'id'],
+                    'type': df_existing.loc[i, 'type'],
+                    'query': df_existing.loc[i, 'query'], 
+                    'name': df_existing.loc[i, 'name'],
+                    'url': df_existing.loc[i, 'url'], 
+                    'license': df_existing.loc[i, 'license'],  
+                    'username': df_existing.loc[i, 'username']
+                    })
+    i +=1 
+    print(f'Starting from index {i}')
+    ids_list = df_existing['id'].tolist()
 
+for word in words: 
 
-# RUN1 STARTED AT 0
-for i in tqdm.tqdm(range(len(df))): 
-    if not df.loc[i, 'downloaded']:
+    os.makedirs('downloads/'+word, exist_ok=True)
+
+    try:
+        results_pager = client.search(
+            query=word,
+            sort="rating_desc",
+            fields="id,type,name,download,license,username,duration"
+        )
+    except:
+        print(f'Error searching for word {word}, skipping it.')
+        continue    
+
+    while results_pager.next is not None:
+        for sound in tqdm.tqdm(results_pager):
+            if sound.duration < 480.0:
+                if sound.id not in ids_list:
+                    if sound.license in licenses:
+                        try:
+                            filename = sound.name.replace(" ", "_").replace("/", "_")
+                            if filename[-3:] not in ['wav', 'aif', 'iff', 'ogg', 'mp3', 'm4a', 'lac']:
+                                filename = filename + '.' + sound.type
+                            g = oauth.get(sound.download)
+
+                            with open('downloads/'+ word +'/'+'{:07d}'.format(i)+'_'+filename, "wb") as f:
+                                for chunk in g.iter_content():
+                                    if chunk:
+                                        f.write(chunk)
+                            data.append({
+                                        'id': sound.id,
+                                        'type': sound.type,
+                                        'query': word, 
+                                        'name': filename, 
+                                        'url': sound.download, 
+                                        'license': sound.license, 
+                                        'username': sound.username})
+                            df = pd.DataFrame(data)
+                            df.to_excel('fsd_crowd_sounds.xlsx')#, index=False)
+                            print(f'Downloaded {i+1} sounds so far...')
+                            i += 1
+                            ids_list.append(sound.id)
+                        except:
+                            print('Error downloading sound with id '+str(sound.id))
         try:
-            g = oauth.get(df.iloc[i].url)
+            results_pager = results_pager.next_page()
         except:
-            print('error querying the API')
-            print(df.iloc[i].url)
-            print(' ')
-            continue
-        try:
-            df.loc[i, 'name'] = df.loc[i, 'name'].replace(" ", "_").replace("/", "_")
-            if '.' in str(df.loc[i, 'name']):
-                filename = df.loc[i, 'name']
-            else:
-                filename = g.headers['Content-Disposition'].split('filename="')[1:][0][:-1].replace(" ", "_").replace("/", "_")
-        except:
-            query_page = 'https://freesound.org/apiv2/sounds/'+str(df.loc[i,'url'].split('/')[5])+'/'
-            fitype = oauth.get(query_page).json()['type']
-            filename = df.loc[i, 'name'] + '.' + fitype
-        try:
-            with open('downloads/'+df.iloc[i].word+'/'+'{:07d}'.format(df.iloc[i].rating_pos)+'_'+filename, "wb") as f:
-                for chunk in g.iter_content():
-                    if chunk:
-                        f.write(chunk)
-            df.loc[i, 'downloaded']= True
-            df.loc[i, 'filenames'] = filename
-        except:
-            print('error storing file')
-            print('downloads/'+df.iloc[i].word+'/'+'{:07d}'.format(df.iloc[i].rating_pos)+'_'+filename)
-            continue
-    else:
-        print('exists already')
+            print('Error advancing to next page, going to next word.')
+            break
 
-df.to_csv('fsd_crowd_sounds.csv', index=False)
+    df = pd.DataFrame(data)
+    df.to_excel('fsd_crowd_sounds.xlsx')#, index=False)
+
+print('Total number of crowd sounds: '+str(len(ids_list)))
+print('DONE!')
